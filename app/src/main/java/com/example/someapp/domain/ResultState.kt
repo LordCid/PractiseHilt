10ece016bfa473ac5f8510a36eb4b3ac16955d2c
Albert Cid <>
2021-10-05 15:58:54 +0200

    Added extension function to Result state

--- app/src/main/java/com/example/someapp/domain/ResultState.kt
+++ app/src/main/java/com/example/someapp/domain/ResultState.kt
@@ -1,6 +1,14 @@
 package com.example.someapp.domain
 
 sealed class ResultState<out T> {
-    class Success<T>(val data: T): ResultState<T>()
-    object Error: ResultState<Nothing>()
+    class Success<T>(val data: T) : ResultState<T>()
+    object Error : ResultState<Nothing>()
+}
+
+
+inline fun <T> ResultState<T>.fold(onSuccess: (T) -> Unit, onError: () -> Unit) {
+    when (this) {
+        is ResultState.Success -> onSuccess(data)
+        is ResultState.Error -> onError()
+    }
 }
